e190fd45e9b3c88f5c03b13c746e5861eda45b9b
Стройка <122112692+stroika-store-ru@users.noreply.github.com>
2023-06-03 15:48:54 +0700

    config.php

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -1,16 +1,16 @@
 <?php
-  //  require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
+   require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
 
 // ***SMTP*** //
-		  require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
-		  const HOST = 'smtp.timeweb.ru';
-	 const LOGIN = 'craft.21.play@gmail.com';
-	  const PASS = 'sonic202307032003';
-		  const PORT = '2525';
+		 // require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
+		//  const HOST = 'smtp.timeweb.ru';
+	// const LOGIN = 'craft.21.play@gmail.com';
+	//  const PASS = 'sonic202307032003';
+	//	  const PORT = '2525';
 //***/SMTP***//
    
-   // const SENDER = 'craft.21.play@gmail.com';
-  //  const CATCHER = 'bossbogdan2003@mail.ru';
-  //  const SUBJECT = 'Заявка с сайта';
-   // const CHARSET = 'UTF-8';
+    const SENDER = 'craft.21.play@gmail.com';
+   const CATCHER = 'bossbogdan2003@mail.ru';
+   const SUBJECT = 'Заявка с сайта';
+    const CHARSET = 'UTF-8';
     
