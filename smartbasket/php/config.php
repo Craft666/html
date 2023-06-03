fb2e8478ce8d9e0de6af22e5dedfa7856f9ea9e1
Стройка <122112692+stroika-store-ru@users.noreply.github.com>
2023-06-03 12:17:16 +0700

    config.php

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -5,7 +5,7 @@
 		  require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
 		  const HOST = 'smtp.mail.ru';
 		 const LOGIN = 'bossbogdan2003@mail.ru';
-		  const PASS = 'cykableat007';
+		  const PASS = 'wKgcX3Adf3Yn7HSimxSY';
 		  const PORT = '465';
 ***SMTP***
    
