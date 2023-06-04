8153193dd100ca3d6f7e92aac374fa07f820e587
Стройка <122112692+stroika-store-ru@users.noreply.github.com>
2023-06-04 17:25:46 +0700

    Add files via upload

---
+++ smartbasket/php/config.php
@@ -0,0 +1,16 @@
+<?php
+//require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
+
+// ***SMTP*** //
+ //   require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
+ //   const HOST = 'smtp.mail.ru';
+ //   const LOGIN = 'bossbogdan2003@mail.ru';
+ //   const PASS = 'wKgcX3Adf3Yn7HSimxSY';
+ //   const PORT = '465';
+//***/SMTP***//
+   
+ //const SENDER = 'bossbogdan2003@mail.ru';
+// const CATCHER = 'dasasxzxw1e12@mail.ru';
+ //const SUBJECT = 'Заявка с сайта';
+ // const CHARSET = 'UTF-8';
+    
